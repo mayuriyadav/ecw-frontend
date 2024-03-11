ac5c1acbc4e449bbca4418f3bb91b7e4ca7429d8
Mayuri Yadav <mayuriyadav7430@gmail.com>
2024-03-11 14:58:42 +0530

    today add navigation

---
+++ src/Coustmer/Components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainCarouselData = () => {
+  return (
+    <div>MainCarouselData</div>
+  )
+}
+
+export default MainCarouselData
---
+++ src/Coustmer/Components/Pages/HomePages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../HomeCarousel/MainCarousel'
+
+const Homepage = () => {
+  return (
+    <div>
+        <MainCarousel></MainCarousel>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
